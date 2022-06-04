a51bf0c3e270a25cdccc1706a9c5f534e9413480
luka <lukakurdadze2@gmail.com>
2022-06-05 00:48:36 +0400

    feat: add special attack functionality

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
-    data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-        }
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' };
     },
-    computed: {
-        mosterBarStyles() {
-            return { width: this.monsterHealth + '%' }
-        },
-        playerBarStyles() {
-            return { width: this.playerHealth + '%' }
-        }
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
     },
-    methods: {
-        attackMonster() {
-            const attackValue = getRandomValue(5, 12)
-            this.monsterHealth -= attackValue;
-            this.attackPlayer();
-        },
-        attackPlayer() {
-            const attackValue = getRandomValue(8, 15)
-            this.playerHealth -= attackValue;
-        }
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    }
+  },
+  methods: {
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    }
+  },
 });
 
-app.mount('#game')
+app.mount('#game');
