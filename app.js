38d3e333142a9ab2f4f5b41221cdd502426cf8c6
luka <lukakurdadze2@gmail.com>
2022-06-05 20:55:13 +0400

    feat: add surrender

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = Vue.createApp({
       }
     },
     monsterHealth(value) {
-      if (value <= 0 && this.monsterHealth <= 0) {
+      if (value <= 0 && this.playerHealth <= 0) {
         this.winner = 'draw'
       } else if (value <= 0) {
         this.winner = 'player'
@@ -29,9 +29,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -39,6 +45,12 @@ const app = Vue.createApp({
     }
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100
+      this.monsterHealth = 100
+      this.currentRound = 0
+      this.winner = null
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -64,6 +76,9 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'monster'
     }
   },
 });
